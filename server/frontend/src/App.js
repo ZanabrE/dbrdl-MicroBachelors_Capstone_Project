0a182dcfc017120bb1189bc8ef7ce403af3688fb
ZanabrE <ernesto.zanabria1979@gmail.com>
2024-01-06 13:06:54 -0500

    modified: ../frontend/src/App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -40,13 +40,15 @@ class App extends React.Component {
               <li class="nav-item active">
                 <a class="nav-link" href="#" onClick={this.setPageHome}>Home <span class="sr-only">(current)</span></a>
               </li>
-                /*
-                Add more tabs and action items for the tabs to give the dealership website a real feel. 
-                */
-        
+
+              <li class="nav-item">
+                <a class="nav-link" href="#" onClick={this.setPageAboutUs}>About Us</a>
+              </li>
+              
               <li class="nav-item">
                 <a class="nav-link" href="#" onClick={this.setPageContactUs}>Contact Us</a>
               </li>
+
             </ul>
           </div>
         </nav>
