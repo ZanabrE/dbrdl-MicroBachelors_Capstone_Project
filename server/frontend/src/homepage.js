a5d0b9d50fcf458259a7926e140b87901f3eb461
ZanabrE <ernesto.zanabria1979@gmail.com>
2024-01-06 13:19:30 -0500

    modified: ../frontend/src/homepage.js

--- server/frontend/src/homepage.js
+++ server/frontend/src/homepage.js
@@ -6,10 +6,11 @@ class HomePage extends React.Component {
     return (
       <div>
         <div style={{display:"flex"}}>
-        <img src='/cardealership.jpeg' style={{width: "300px"}}/>
-        <div style={{margin:"100px"}}>
-             <span style={{margin:"60px"}}>
+        <img src='/cardealership.jpeg' style={{width: "600px", length: "400px"}}/>
+        <div style={{margin:"80px"}}>
+             <span style={{margin:"200px"}}>
              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+             "This is the home page."
                </span >
            </div>
 
